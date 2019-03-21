1641e15783470e6ae94f842a888f046233126a77
prmorais13 <prmorais_13@hotmail.com>
2019-03-21 11:12:30 -0300

    Conclusão do projeto

--- controllers/controleProduto.js
+++ controllers/controleProduto.js
@@ -2,7 +2,11 @@ const { produtos } = require('../db.json');
 
 module.exports = {
   getProdutos: (req, res) => {
-    res.json(produtos);
+    if (produtos.length > 0) {
+      res.json(produtos);
+    } else {
+      res.json({ success: false, msg: 'Não há dados' });
+    }
   },
   addProdutos: (req, res) => {
     const { nome } = req.body;
@@ -14,24 +18,23 @@ module.exports = {
     res.json({ success: true, msg: 'Dados gravados.' });
   },
   updateProduto: (req, res) => {
-    const id = req.params.id;
+    const { id } = req.params;
     const { nome } = req.body;
 
     produtos.forEach(produto => {
       if (produto.id === Number(id)) {
         produto.nome = nome;
       }
-      res.json({ success: true, msg: 'Dados atualizados.' });
     });
+    res.json({ success: true, msg: 'Dados atualizados.' });
   },
   delProduto: (req, res) => {
-    const { id } = req.params.id;
+    const { id } = req.params;
     produtos.forEach((produto, i) => {
       if (produto.id === Number(id)) {
-        // console.log(id + '-' + i);
         produtos.splice(i, 1);
       }
-      res.json({ success: true, msg: 'Dados excluídos.' });
     });
+    res.json({ success: true, msg: 'Dados excluídos.' });
   }
 };
